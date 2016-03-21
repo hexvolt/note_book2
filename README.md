10f64317122f36bbe29220456c283ddaee778960
hexvolt <hexvolt@gmail.com>
2016-03-21 23:56:44 +0200

    ticket:2 debug toolbar added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # note_book2
-A note book implementation based on Python3.5 asyncio and Angular2
+A notebook implementation based on Python3.5 asyncio and Angular2
---
+++ app.py
@@ -0,0 +1,16 @@
+import asyncio
+
+import aiohttp_debugtoolbar
+from aiohttp_debugtoolbar import toolbar_middleware_factory
+from aiohttp import web
+
+
+async def hello(request):
+    return web.Response(body=b"Hello, world")
+
+app = web.Application(middlewares=[toolbar_middleware_factory])
+aiohttp_debugtoolbar.setup(app)
+
+app.router.add_route('GET', '/', hello)
+
+web.run_app(app)
